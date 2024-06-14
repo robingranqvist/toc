bae8a856682c5763fa654e3e74d9107f2cb9bc04
Robin Granqvist <robin.granqvist@hotmail.se>
2024-06-14 10:52:42 +0300

    init

---
+++ script.js
@@ -0,0 +1,39 @@
+class Toc {
+  constructor(
+    textSource,
+    targetDiv,
+    headingTypes = ["h1", "h2", "h3", "h4", "h5", "h6"],
+    indentSize = 0
+  ) {
+    this.textSource = textSource;
+    this.targetDiv = targetDiv;
+    this.headingTypes = headingTypes;
+    this.indentSize = indentSize;
+
+    if (!textSource || !targetDiv) {
+      throw new Error("Both a text source and a target div are required");
+    }
+
+    this.generate();
+  }
+
+  generate() {
+    const textSource = document.querySelectorAll(`.${this.textSource}`);
+    let html = "<ul class='toc'>";
+    let count = 0;
+
+    textSource.forEach((div) => {
+      div.querySelectorAll(this.headingTypes.join(",")).forEach((heading) => {
+        const id = `toc-h${count++}`;
+        const level = parseInt(heading.tagName.substring(1)) - 1;
+
+        heading.id = id;
+        html += `<li class='toc-list-item' style="margin-left: ${
+          level * this.indentSize
+        }px;"><a href="#${id} class='${id}'">${heading.textContent}</a></li>`;
+      });
+    });
+
+    document.querySelector(`.${this.targetDiv}`).innerHTML = html;
+  }
+}
